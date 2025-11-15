f5318f9329dd95f031ce4a23e66d4ce1dec9f5ba
Caballec <122234465+Caballec@users.noreply.github.com>
2025-11-15 12:13:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,3 @@ window.addEventListener("scroll", () => {
     header.style.backgroundColor = "#6e1b1b";
   }
 });
-
-rules_version = '2';
-service firebase.storage {
-  match /b/{bucket}/o {
-    match /{allPaths=**} {
-      allow read: if false; // users cannot read files
-      allow write: if request.auth == null || request.auth != null; // public write
-    }
-  }
-}
