2c14a62e69b8282da2c0ecfd04961d6ae78785f7
Caballec <122234465+Caballec@users.noreply.github.com>
2025-11-15 12:11:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ window.addEventListener("scroll", () => {
   }
 });
 
+rules_version = '2';
+service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read: if false; // users cannot read files
+      allow write: if request.auth == null || request.auth != null; // public write
+    }
+  }
+}
